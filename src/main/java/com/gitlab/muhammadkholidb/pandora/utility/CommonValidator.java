b6b8e4426cde954b939a262f6d2a20331189b06c
Muhammad <muhammadkholidb@gmail.com>
2021-05-23 17:06:08 +0700

    fix validator for non required fields

--- src/main/java/com/gitlab/muhammadkholidb/pandora/utility/CommonValidator.java
+++ src/main/java/com/gitlab/muhammadkholidb/pandora/utility/CommonValidator.java
@@ -5,6 +5,7 @@ import org.apache.commons.validator.routines.DomainValidator;
 import org.apache.commons.validator.routines.EmailValidator;
 import org.controlsfx.validation.Severity;
 import org.controlsfx.validation.ValidationResult;
+import org.controlsfx.validation.ValidationSupport;
 import org.controlsfx.validation.Validator;
 
 public class CommonValidator {
@@ -22,8 +23,15 @@ public class CommonValidator {
     }
 
     public static Validator<String> createEmailValidator(final String message, final Severity severity) {
-        return (control, value) -> ValidationResult.fromMessageIf(control, message, severity,
-                !EmailValidator.getInstance().isValid(value));
+        return (control, value) -> {
+            boolean condition;
+            if (ValidationSupport.isRequired(control)) {
+                condition = !EmailValidator.getInstance().isValid(value);
+            } else {
+                condition = StringUtils.isNotBlank(value) && !EmailValidator.getInstance().isValid(value);
+            }
+            return ValidationResult.fromMessageIf(control, message, severity, condition);
+        };
     }
 
     public static Validator<String> createEmailValidator(final String message) {
@@ -31,8 +39,15 @@ public class CommonValidator {
     }
 
     public static Validator<String> createDomainValidator(final String message, final Severity severity) {
-        return (control, value) -> ValidationResult.fromMessageIf(control, message, severity,
-                !DomainValidator.getInstance().isValid(value));
+        return (control, value) -> {
+            boolean condition;
+            if (ValidationSupport.isRequired(control)) {
+                condition = !DomainValidator.getInstance().isValid(value);
+            } else {
+                condition = StringUtils.isNotBlank(value) && !DomainValidator.getInstance().isValid(value);
+            }
+            return ValidationResult.fromMessageIf(control, message, severity, condition);
+        };
     }
 
     public static Validator<String> createDomainValidator(final String message) {
